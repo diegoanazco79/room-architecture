cdcbdf5b7f9d5c4499113480f769ca658901b946
diegoanazco79 <djanazcob@gmail.com>
2023-07-27 15:21:54 -0500

    :lipstick: COSMETIC :: Change classname && for ternary

--- src/app/main-layout/Header.tsx
+++ src/app/main-layout/Header.tsx
@@ -29,7 +29,7 @@ export default function Header () {
   const pathname = usePathname()
 
   return (
-    <nav className={`fixed top-0 left-0 z-20 w-full bg-white ${scrolling && 'shadow-sm'}`}>
+    <nav className={`fixed top-0 left-0 z-20 w-full bg-white ${scrolling ? 'shadow-sm' : ''}`}>
       <div className='flex flex-wrap items-center justify-between max-w-screen-xl px-4 py-6 mx-auto'>
         <a href='/' className='flex items-center'>
           <Image
@@ -44,7 +44,7 @@ export default function Header () {
             {menuItems.map((item, idx) => (
               <li key={idx}>
                 <Link
-                  // className={`block ${pathname === item.url && 'bg-black text-white'} hover:bg-black hover:text-white hover:p-2 p-2 hover:transition-all hover:duration-300 text-black`}
+                  className={`block ${pathname === item.url ? 'bg-black text-white' : ''} hover:bg-black hover:text-white hover:p-2 p-2 hover:transition-all hover:duration-300 text-black`}
                   href={item.url ?? '/'}
                 >
                   {item.label ?? 'Home'}
