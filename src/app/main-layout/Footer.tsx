27b37af2bbc46b4933c8ecf81738000a909cffa7
diegoanazco79 <djanazcob@gmail.com>
2023-07-27 13:18:46 -0500

    :lipstick: COSMETIC :: Responsive for footer

--- src/app/main-layout/Footer.tsx
+++ src/app/main-layout/Footer.tsx
@@ -7,42 +7,40 @@ import { FacebookFilled, InstagramFilled, LinkedinFilled } from '@ant-design/ico
 
 export default function Footer () {
   return (
-    <footer className='py-12 bg-black '>
+    <footer className='py-12 bg-black'>
       <div className='w-full max-w-screen-xl px-4 mx-auto'>
-        <div className='md:flex md:justify-between'>
-          <div className='grid w-full grid-cols-2 gap-8'>
-            <div className='flex flex-col justify-start'>
-              <Image
-                src='/img/logo-white.png'
-                width={140}
-                height={140}
-                alt='ROOM Arquitectos'
-              />
-              <p className='w-4/5 mt-4 text-xs'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-            <div className='flex flex-col items-end justify-start'>
-              <h1 className='flex items-center font-bold text-lg h-[52.5px] my-auto'>Contacto</h1>
-              <div className='flex flex-col items-end mt-4'>
-                <span className='mb-3 text-xs'>+51 999 999 999</span>
-                <span className='mb-3 text-xs'>roomArquitectos@room.com</span>
-                <span className='text-xs'>Calle Los Cóndores 123 - Urb. Valle Hermoso - Cerro Colarado</span>
-                <span className='text-xs'>Arequipa - Perú</span>
-              </div>
+        <div className='grid w-full grid-cols-1 gap-8 md:grid-cols-2'>
+          <div className='flex flex-col items-center md:items-start md:justify-start'>
+            <Image
+              src='/img/logo-white.png'
+              width={140}
+              height={140}
+              alt='ROOM Arquitectos'
+            />
+            <p className='w-4/5 mt-4 text-xs'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+              text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+          <div className='flex flex-col items-center md:items-end md:justify-start'>
+            <h1 className='flex items-center font-bold text-lg md:h-[52.5px] h-[40px] my-auto'>Contacto</h1>
+            <div className='flex flex-col items-center mt-4 md:items-end'>
+              <span className='mb-3 text-xs'>+51 999 999 999</span>
+              <span className='mb-3 text-xs'>roomArquitectos@room.com</span>
+              <span className='text-xs'>Calle Los Cóndores 123 - Urb. Valle Hermoso - Cerro Colorado</span>
+              <span className='text-xs'>Arequipa - Perú</span>
             </div>
           </div>
         </div>
-        <hr className='my-3 border-[#414141]' />
       </div>
-      <div className='w-full max-w-screen-xl px-4 pt-2 mx-auto'>
-        <div className='md:flex md:justify-between'>
-          <div className='grid w-full grid-cols-2 gap-8'>
-            <div className='flex flex-col justify-start'>
-              <span className='text-xs'>© 2023 ROOM Arquitectos</span>
-            </div>
-            <div className='flex justify-end'>
+      <div className='w-full max-w-screen-xl px-4 mx-auto'>
+        <hr className='my-6 border-[#414141]' />
+        <div className='grid w-full grid-cols-1 gap-4 md:grid-cols-2'>
+          <div className='flex flex-col items-center md:items-start md:justify-start'>
+            <span className='text-xs'>© 2023 ROOM Arquitectos</span>
+          </div>
+          <div className='flex flex-col items-center md:items-end md:justify-start'>
+            <div>
               <a className='mr-4 cursor-pointer'>
                 <FacebookFilled style={{ fontSize: 20 }} />
               </a>
