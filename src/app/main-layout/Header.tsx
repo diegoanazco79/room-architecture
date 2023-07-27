b93e40af085d65796c66762cd9709cde58e67a52
diegoanazco79 <djanazcob@gmail.com>
2023-07-27 15:15:16 -0500

    :bug: BUG FIX :: Comment a drawer

--- src/app/main-layout/Header.tsx
+++ src/app/main-layout/Header.tsx
@@ -2,29 +2,27 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { Drawer } from 'antd'
+// import { Drawer } from 'antd'
 import { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation'
 
-import { MenuOutlined, CloseOutlined } from '@ant-design/icons'
+import { menuItems } from '../helpers/menu-items'
+
+import { MenuOutlined } from '@ant-design/icons'
 
 export default function Header () {
   const [showNavbar, setShowNavbar] = useState(false)
   const [scrolling, setScrolling] = useState(false)
 
   const handleScroll = () => {
-    if (typeof window !== 'undefined') {
-      if (window.scrollY > 0) setScrolling(true)
-      else setScrolling(false)
-    }
+    if (window.scrollY > 0) setScrolling(true)
+    else setScrolling(false)
   }
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('scroll', handleScroll)
-      return () => {
-        window.removeEventListener('scroll', handleScroll)
-      }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
     }
   }, [])
 
@@ -43,24 +41,16 @@ export default function Header () {
         </a>
         <div className='hidden w-full md:block md:w-auto'>
           <ul className='flex items-center space-x-8 font-medium'>
-            <Link
-              className={`block ${pathname === '/' && 'bg-black text-white'} hover:bg-black hover:text-white hover:p-2 p-2 hover:transition-all hover:duration-300 text-black`}
-              href='/'
-            >
-              Inicio
-            </Link>
-            <Link
-              className={`block ${pathname === '/projects' && 'bg-black text-white'} hover:bg-black hover:text-white hover:p-2 p-2 hover:transition-all hover:duration-300 text-black`}
-              href='/projects'
-            >
-              Projects
-            </Link>
-            <Link
-              className={`block ${pathname === '/about' && 'bg-black text-white'} hover:bg-black hover:text-white hover:p-2 p-2 hover:transition-all hover:duration-300 text-black`}
-              href='/about'
-            >
-              Nosotros
-            </Link>
+            {menuItems.map((item, idx) => (
+              <li key={idx}>
+                <Link
+                  className={`block ${pathname === item.url && 'bg-black text-white'} hover:bg-black hover:text-white hover:p-2 p-2 hover:transition-all hover:duration-300 text-black`}
+                  href={item.url}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         <button
@@ -70,7 +60,7 @@ export default function Header () {
           <MenuOutlined style={{ fontSize: 20 }} />
         </button>
       </div>
-      <Drawer closable={false} placement='right' onClose={() => setShowNavbar(false)} open={showNavbar}>
+      {/* <Drawer closable={false} placement='right' onClose={() => setShowNavbar(false)} open={showNavbar}>
         <div className='flex justify-end'>
           <button
             className='p-2 rounded-lg text-gray-950 md:hidden hover:bg-gray-100'
@@ -80,26 +70,18 @@ export default function Header () {
           </button>
         </div>
         <ul>
-          <Link
-            className={`block ${pathname === '/' ? 'bg-black text-white' : 'text-gray-950'} py-4 font-medium pl-2`}
-            href='/'
-          >
-            Inicio
-          </Link>
-          <Link
-            className={`block ${pathname === '/projects' ? 'bg-black text-white' : 'text-gray-950'} py-4 font-medium pl-2`}
-            href='/projects'
-          >
-            Proyectos
-          </Link>
-          <Link
-            className={`block ${pathname === '/about' ? 'bg-black text-white' : 'text-gray-950'} py-4 font-medium pl-2`}
-            href='/about'
-          >
-            Nosotros
-          </Link>
+          {menuItems.map((item, idx) => (
+            <li className='rounded-lg' key={idx} onClick={() => setShowNavbar(false)}>
+              <Link
+                className={`block ${pathname === item.url ? 'bg-black text-white' : 'text-gray-950'} py-4 font-medium pl-2`}
+                href={item.url}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
         </ul>
-      </Drawer>
+      </Drawer> */}
     </nav>
   )
 }
