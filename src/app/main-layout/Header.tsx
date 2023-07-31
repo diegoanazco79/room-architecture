028ff6a449a5b063327db06e68d24203b819fa67
diegoanazco79 <djanazcob@gmail.com>
2023-07-31 15:45:24 -0500

    fix/delete-image-header

--- src/app/main-layout/Header.tsx
+++ src/app/main-layout/Header.tsx
@@ -11,85 +11,44 @@ import { MenuOutlined, CloseOutlined } from '@ant-design/icons'
 import { menuItems } from '../helpers/menu-items'
 
 export default function Header () {
-  // const pathname = usePathname()
+  const pathname = usePathname()
 
-  // const [showNavbar, setShowNavbar] = useState(false)
-  // const [scrolling, setScrolling] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(false)
+  const [scrolling, setScrolling] = useState(false)
 
-  // const handleScroll = () => {
-  //   if (typeof window !== 'undefined') {
-  //     if (window.scrollY > 0) setScrolling(true)
-  //     else setScrolling(false)
-  //   }
-  // }
+  const handleScroll = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > 0) setScrolling(true)
+      else setScrolling(false)
+    }
+  }
 
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined') {
-  //     window.addEventListener('scroll', handleScroll)
-  //     return () => {
-  //       window.removeEventListener('scroll', handleScroll)
-  //     }
-  //   }
-  // }, [])
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleScroll)
+      return () => {
+        window.removeEventListener('scroll', handleScroll)
+      }
+    }
+  }, [])
 
   return (
-    <>
-    <ul>
-      {menuItems?.map((item, idx) => (
-      <li key={item.url}>
-        <Link
-          href={item.url}
-        >
-          {item.label}
-        </Link>
-      </li>
-      ))}
-    </ul>
-      {/* <nav className={`fixed top-0 left-0 z-20 w-full bg-white ${scrolling ? 'shadow-sm' : ''}`}>
-        <div className='flex flex-wrap items-center justify-between max-w-screen-xl px-4 py-6 mx-auto'>
-          <a href='/' className='flex items-center'>
-            <Image
-              src='/img/logo.png'
-              width={140}
-              height={140}
-              alt='ROOM Arquitectos'
-            />
-          </a>
-          <div className='hidden w-full md:block md:w-auto'>
-            <ul className='flex items-center space-x-8 font-medium'>
-              {menuItems?.map((item, idx) => (
-                <li key={idx}>
-                  <Link
-                    className={`block ${pathname === item.url ? 'bg-black text-white' : ''} hover:bg-black hover:text-white hover:p-2 p-2 hover:transition-all hover:duration-300 text-black`}
-                    href={item.url}
-                  >
-                    {item.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <button
-            className='p-2 text-black rounded-lg md:hidden'
-            onClick={() => setShowNavbar(!showNavbar)}
-          >
-            <MenuOutlined style={{ fontSize: 20 }} />
-          </button>
-        </div>
-        <Drawer closable={false} placement='right' onClose={() => setShowNavbar(false)} open={showNavbar}>
-          <div className='flex justify-end'>
-            <button
-              className='p-2 mb-3 text-black rounded-lg md:hidden'
-              onClick={() => setShowNavbar(!showNavbar)}
-            >
-              <CloseOutlined style={{ fontSize: 25 }} />
-            </button>
-          </div>
-          <ul>
+    <nav className={`fixed top-0 left-0 z-20 w-full bg-white ${scrolling ? 'shadow-sm' : ''}`}>
+      <div className='flex flex-wrap items-center justify-between max-w-screen-xl px-4 py-6 mx-auto'>
+        {/* <a href='/' className='flex items-center'>
+          <Image
+            src='/img/logo.png'
+            width={140}
+            height={140}
+            alt='ROOM Arquitectos'
+          />
+        </a> */}
+        <div className='hidden w-full md:block md:w-auto'>
+          <ul className='flex items-center space-x-8 font-medium'>
             {menuItems?.map((item, idx) => (
-              <li className='rounded-lg' key={idx} onClick={() => setShowNavbar(false)}>
+              <li key={idx}>
                 <Link
-                  className={`block ${pathname === item.url ? 'bg-black text-white' : 'text-black'} py-4 font-medium pl-2`}
+                  className={`block ${pathname === item.url ? 'bg-black text-white' : ''} hover:bg-black hover:text-white hover:p-2 p-2 hover:transition-all hover:duration-300 text-black`}
                   href={item.url}
                 >
                   {item.label}
@@ -97,8 +56,36 @@ export default function Header () {
               </li>
             ))}
           </ul>
-        </Drawer>
-      </nav> */}
-    </>
+        </div>
+        <button
+          className='p-2 text-black rounded-lg md:hidden'
+          onClick={() => setShowNavbar(!showNavbar)}
+        >
+          <MenuOutlined style={{ fontSize: 20 }} />
+        </button>
+      </div>
+      <Drawer closable={false} placement='right' onClose={() => setShowNavbar(false)} open={showNavbar}>
+        <div className='flex justify-end'>
+          <button
+            className='p-2 mb-3 text-black rounded-lg md:hidden'
+            onClick={() => setShowNavbar(!showNavbar)}
+          >
+            <CloseOutlined style={{ fontSize: 25 }} />
+          </button>
+        </div>
+        <ul>
+          {menuItems?.map((item, idx) => (
+            <li className='rounded-lg' key={idx} onClick={() => setShowNavbar(false)}>
+              <Link
+                className={`block ${pathname === item.url ? 'bg-black text-white' : 'text-black'} py-4 font-medium pl-2`}
+                href={item.url}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Drawer>
+    </nav>
   )
 }
