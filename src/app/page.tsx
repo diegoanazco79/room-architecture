9b817c7ad1e93a8373e8dd83cfff2aef4896acc6
diegoanazco79 <djanazcob@gmail.com>
2023-07-31 14:55:36 -0500

    :sparkles: NEW FEATURE :: Parallax image in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,17 @@ import MainProjects from './components/MainProjects'
 
 export default function Home () {
   return (
-    <main className='flex flex-col items-center justify-between min-h-screen py-24 text-black bg-white md:py-32 md:px-24'>
-      <div className='flex flex-col items-center w-full max-w-screen-xl px-6 mx-auto lg:px-12 xl:px-16'>
-        <BannerSlider />
-        <MainProjects />
+    <main className='py-24 bg-white md:py-32'>
+      <div className='flex flex-col items-center justify-between min-h-screen overflow-x-hidden text-black bg-white md:px-24'>
+        <div className='flex flex-col items-center w-full max-w-screen-xl px-6 mx-auto lg:px-12 xl:px-16'>
+          <BannerSlider />
+          <MainProjects />
+        </div>
       </div>
+      <section
+        className='h-[600px] bg-fixed bg-center bg-cover hidden md:block'
+        style={{ backgroundImage: 'url(/img/home/slider/full-width.jpg)' }}
+      />
     </main>
   )
 }
