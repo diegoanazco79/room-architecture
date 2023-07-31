aa5c8c119b26a759f4033d9920caf55f9a9cf66f
diegoanazco79 <djanazcob@gmail.com>
2023-07-31 15:50:32 -0500

    fix/show-button-responsive

--- src/app/main-layout/Header.tsx
+++ src/app/main-layout/Header.tsx
@@ -13,27 +13,27 @@ import { menuItems } from '../helpers/menu-items'
 export default function Header () {
   const pathname = usePathname()
 
-  // const [showNavbar, setShowNavbar] = useState(false)
-  // const [scrolling, setScrolling] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(false)
+  const [scrolling, setScrolling] = useState(false)
 
-  // const handleScroll = () => {
-  //   if (typeof window !== 'undefined') {
-  //     if (window.scrollY > 0) setScrolling(true)
-  //     else setScrolling(false)
-  //   }
-  // }
+  const handleScroll = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > 0) setScrolling(true)
+      else setScrolling(false)
+    }
+  }
 
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined') {
-  //     window.addEventListener('scroll', handleScroll)
-  //     return () => {
-  //       window.removeEventListener('scroll', handleScroll)
-  //     }
-  //   }
-  // }, [])
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleScroll)
+      return () => {
+        window.removeEventListener('scroll', handleScroll)
+      }
+    }
+  }, [])
 
   return (
-    <nav className={`fixed top-0 left-0 z-20 w-full bg-white ${true ? 'shadow-sm' : ''}`}>
+    <nav className={`fixed top-0 left-0 z-20 w-full bg-white ${scrolling ? 'shadow-sm' : ''}`}>
       <div className='flex flex-wrap items-center justify-between max-w-screen-xl px-4 py-6 mx-auto'>
         <a href='/' className='flex items-center'>
           <Image
@@ -57,12 +57,12 @@ export default function Header () {
             ))}
           </ul>
         </div>
-        {/* <button
+        <button
           className='p-2 text-black rounded-lg md:hidden'
           onClick={() => setShowNavbar(!showNavbar)}
         >
-          <MenuOutlined style={{ fontSize: 20 }} />
-        </button> */}
+          Menu
+        </button>
       </div>
       {/* <Drawer closable={false} placement='right' onClose={() => setShowNavbar(false)} open={showNavbar}>
         <div className='flex justify-end'>
